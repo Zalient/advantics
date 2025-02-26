15eb32735c47a3860eb5a91d0955193ffa76196a
mh6n23 <mh6n23@soton.ac.uk>
2025-02-26 16:12:52 +0000

    Page switch test

--- src/main/java/com/university/grp20/controller/FileUpload.java
+++ src/main/java/com/university/grp20/controller/FileUpload.java
@@ -2,13 +2,19 @@ package com.university.grp20.controller;
 
 import com.university.grp20.model.FileProcessor;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Alert;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
 
 public class FileUpload {
   private static final Logger logger = LogManager.getLogger(FileUpload.class);
@@ -46,7 +52,7 @@ public class FileUpload {
   @FXML
   private void handleClickUpload() {
     fileChooser.setTitle("Click Log File Upload");
-    File clickFile = fileChooser.showOpenDialog(impressionUpload.getScene().getWindow());
+    File clickFile = fileChooser.showOpenDialog(clickUpload.getScene().getWindow());
 
     if (clickFile != null) {
       logger.info("Click Log File selected with path " + clickFile.getPath());
@@ -60,7 +66,7 @@ public class FileUpload {
   @FXML
   private void handleServerUpload() {
     fileChooser.setTitle("Server Log File Upload");
-    File serverFile = fileChooser.showOpenDialog(impressionUpload.getScene().getWindow());
+    File serverFile = fileChooser.showOpenDialog(serverUpload.getScene().getWindow());
 
     if (serverFile != null) {
       logger.info("Server Log File selected with path " + serverFile.getPath());
@@ -72,13 +78,24 @@ public class FileUpload {
   }
 
   @FXML
-  private void handleNext() {
+  private void handleNext(javafx.event.ActionEvent event) {
     logger.info("next clicked");
 
     Boolean ready = fileProcessor.isReady();
 
-    if (ready) {
+    if (ready) { //replace true w/ ready
       logger.info ("3 file paths done");
+
+      try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Dashboard.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) next.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Advertising Dashboard");
+        stage.show();
+      } catch (IOException e) {
+        logger.info("Error reading FXML file") ;
+      }
     }
     else {
       Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -88,6 +105,4 @@ public class FileUpload {
       alert.showAndWait();
     }
   }
-
-
 }
