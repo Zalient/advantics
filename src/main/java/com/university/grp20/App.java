e6da4c25d3a6c2278c9bda952609f36076cccb72
mh6n23 <mh6n23@soton.ac.uk>
2025-02-26 14:46:08 +0000

    Allowed a file path to be selected for impression file

--- src/main/java/com/university/grp20/App.java
+++ src/main/java/com/university/grp20/App.java
@@ -1,11 +1,19 @@
 package com.university.grp20;
 
+import com.university.grp20.controller.FileUpload;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Simple JavaFX app for the intro page
@@ -13,24 +21,25 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     /**
      * Starts JavaFX app by setting up page with intro message and 3 buttons
      */
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Upload Files");
-        Button impressionButton = new Button("Upload impression");
-        Button clickButton = new Button("Upload click");
-        Button serverButton = new Button("Upload server");
+        logger.info("Starting application");
+        try {
+            FXMLLoader loader = new FXMLLoader(new File("src/main/java/com/university/grp20/view/FileUpload.fxml").toURI().toURL());
+            Parent root = loader.load();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Advertising Dashboard");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        VBox introBox = new VBox(10, label, impressionButton, clickButton, serverButton);
-        introBox.setStyle("-fx-padding:20px; -fx-alignment: center;");
 
-        Scene scene = new Scene(introBox, 400, 300);
-        stage.setScene(scene);
-
-        stage.setTitle("JavaFX App");
-        stage.show();
     }
 
     /**
--- src/main/java/com/university/grp20/controller/FileUpload.java
+++ src/main/java/com/university/grp20/controller/FileUpload.java
@@ -1,4 +1,33 @@
 package com.university.grp20.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.stage.FileChooser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
 public class FileUpload {
+  private static final Logger logger = LogManager.getLogger(FileUpload.class);
+
+  @FXML
+  private Button impressionUpload;
+
+  @FXML
+  private void handleImpressionUpload() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Impression Upload");
+    File impressionFile = fileChooser.showOpenDialog(impressionUpload.getScene().getWindow());
+
+    if (impressionFile != null) {
+      logger.info("Impression file selected at" + impressionFile.getPath());
+    }
+    else {
+      logger.info("File upload aborted");
+    }
+
+
+  }
 }
