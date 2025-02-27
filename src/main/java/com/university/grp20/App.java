f1f774c96c47f0160cf4e2eeb7eb92807ca99706
Brandon Wong Ze Rong <brandonwongzr@gmail.com>
2025-02-27 14:32:42 +0000

    Fixed pom.xml and App

--- src/main/java/com/university/grp20/App.java
+++ src/main/java/com/university/grp20/App.java
@@ -1,26 +1,51 @@
 package com.university.grp20;
 
+import com.university.grp20.controller.FileUpload;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.Objects;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Simple JavaFX app for the intro page
  * intro page contains an intro message with 3 buttons
  */
 public class App extends Application {
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    /**
+     * Starts JavaFX app by setting up page with intro message and 3 buttons
+     *
+     */
     @Override
     public void start(Stage stage) {
+        logger.info("Starting application");
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FileUpload.fxml"));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Advertising Dashboard");
+            stage.show();
+        } catch (IOException e) {
+            logger.info("Error reading FXML file");
+        }
+
+
     }
 
     /**
      * Main method launches the application
-     */
+     * */
     public static void main(String[] args) {
-        launch();
-    }
+        launch();}
 }
