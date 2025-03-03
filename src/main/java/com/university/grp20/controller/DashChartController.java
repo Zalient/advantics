35c4a043ea47587b7c3cc669790bd12e9e8057ff
Ze010 <159085392+Ze010@users.noreply.github.com>
2025-03-03 03:33:38 +0000

    feat: Display unlimited charts

--- src/main/java/com/university/grp20/controller/DashChartController.java
+++ src/main/java/com/university/grp20/controller/DashChartController.java
@@ -9,6 +9,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.embed.swing.SwingNode;
@@ -25,7 +26,7 @@ public class DashChartController {
     public MenuItem impressionsChart, clicksChart, uniquesChart, bouncesChart, conversionsChart, totalCostChart, ctrChart, cpaChart, cpcChart, cpmChart, bounceRateChart;
 
     @FXML
-    private GridPane chartDisplayGrid;
+    private FlowPane chartDisplayFlowPane;
 
     private Stage stage;
     private Scene scene;
@@ -61,63 +62,56 @@ public class DashChartController {
     public void displayImpressionsChart(){
         JFreeChart chart = generateChart.impressionsChart();
         ChartViewer chartViewer = new ChartViewer(chart);
-        int col = numOfCharts % 2;
-        int row = numOfCharts / 2;
-        chartDisplayGrid.add(chartViewer, col, row);
+        chartViewer.setPrefSize(755, 400);
+        chartDisplayFlowPane.getChildren().add(chartViewer);
         numOfCharts++;
     }
 
     public void displayClicksChart(){
         JFreeChart chart = generateChart.clicksChart();
         ChartViewer chartViewer = new ChartViewer(chart);
-        int col = numOfCharts % 2;
-        int row = numOfCharts / 2;
-        chartDisplayGrid.add(chartViewer, col, row);
+        chartViewer.setPrefSize(755, 400);
+        chartDisplayFlowPane.getChildren().add(chartViewer);
         numOfCharts++;
     }
 
     public void displayUniquesChart(){
         JFreeChart chart = generateChart.uniquesChart();
         ChartViewer chartViewer = new ChartViewer(chart);
-        int col = numOfCharts % 2;
-        int row = numOfCharts / 2;
-        chartDisplayGrid.add(chartViewer, col, row);
+        chartViewer.setPrefSize(755, 400);
+        chartDisplayFlowPane.getChildren().add(chartViewer);
         numOfCharts++;
     }
 
     public void displayBouncesChart(){
         JFreeChart chart = generateChart.bouncesChart();
         ChartViewer chartViewer = new ChartViewer(chart);
-        int col = numOfCharts % 2;
-        int row = numOfCharts / 2;
-        chartDisplayGrid.add(chartViewer, col, row);
+        chartViewer.setPrefSize(755, 400);
+        chartDisplayFlowPane.getChildren().add(chartViewer);
         numOfCharts++;
     }
 
     public void displayConversionsChart(){
         JFreeChart chart = generateChart.conversionsChart();
         ChartViewer chartViewer = new ChartViewer(chart);
-        int col = numOfCharts % 2;
-        int row = numOfCharts / 2;
-        chartDisplayGrid.add(chartViewer, col, row);
+        chartViewer.setPrefSize(755, 400);
+        chartDisplayFlowPane.getChildren().add(chartViewer);
         numOfCharts++;
     }
 
     public void displayTotalCostChart(){
         JFreeChart chart = generateChart.totalCostChart();
         ChartViewer chartViewer = new ChartViewer(chart);
-        int col = numOfCharts % 2;
-        int row = numOfCharts / 2;
-        chartDisplayGrid.add(chartViewer, col, row);
+        chartViewer.setPrefSize(755, 400);
+        chartDisplayFlowPane.getChildren().add(chartViewer);
         numOfCharts++;
     }
 
     public void displayCTRChart(){
         JFreeChart chart = generateChart.ctrChart();
         ChartViewer chartViewer = new ChartViewer(chart);
-        int col = numOfCharts % 2;
-        int row = numOfCharts / 2;
-        chartDisplayGrid.add(chartViewer, col, row);
+        chartViewer.setPrefSize(755, 400);
+        chartDisplayFlowPane.getChildren().add(chartViewer);
         numOfCharts++;
     }
 }
