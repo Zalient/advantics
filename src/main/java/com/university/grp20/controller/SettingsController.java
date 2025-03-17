21d07004ead5c376b9a0d08b9452c3f373b39f86
Alex Peaty <alexpeaty1@gmail.com>
2025-03-17 12:56:07 +0000

    feat: adding timestamps to the title of exported pdf and csv documents for the user operation logs

--- src/main/java/com/university/grp20/controller/SettingsController.java
+++ src/main/java/com/university/grp20/controller/SettingsController.java
@@ -5,16 +5,22 @@ import com.university.grp20.model.ExportLogService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SettingsController {
 
     private ExportLogService exportLogService = new ExportLogService();
     private OperationLogger operationLogger = new OperationLogger();
 
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+
     @FXML
     public void handleExportLogToPDF(ActionEvent event) {
         String logFileName = operationLogger.getLogFileName();
-        String filePath = "operationLogs/exported_log.pdf";
+        String timestamp = LocalDateTime.now().format(formatter);
+        //Gives the timestamp in the filename once exported
+        String filePath = "operationLogs/exported_log_" + timestamp + ".pdf";
         exportLogService.exportLogToPDF(logFileName, filePath);
         showAlert("Success", "Log exported to PDF successfully.\nSaved at: " + filePath);
     }
@@ -22,7 +28,9 @@ public class SettingsController {
     @FXML
     public void handleExportLogToCSV(ActionEvent event) {
         String logFileName = operationLogger.getLogFileName();
-        String filePath = "operationLogs/exported_log.csv";
+        String timestamp = LocalDateTime.now().format(formatter);
+        //Gives the timestamp in the filename once exported
+        String filePath = "operationLogs/exported_log_" + timestamp + ".csv";
         exportLogService.exportLogToCSV(logFileName, filePath);
         showAlert("Success", "Log exported to CSV successfully.\nSaved at: " + filePath);
     }
