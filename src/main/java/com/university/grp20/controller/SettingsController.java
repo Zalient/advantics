fb94c4f919f283743475f05ee97a952ecc1f1da4
mh6n23 <mh6n23@soton.ac.uk>
2025-03-17 12:46:55 +0000

    Setup settings controller

--- src/main/java/com/university/grp20/controller/SettingsController.java
+++ src/main/java/com/university/grp20/controller/SettingsController.java
@@ -1,3 +1,25 @@
 package com.university.grp20.controller;
 
-public class SettingsController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SettingsController {
+  private final Logger logger = LogManager.getLogger(MetricsController.class);
+
+  @FXML private TextField addUsernameField;
+  @FXML private TextField addPasswordField;
+  @FXML private MenuButton selectRoleMenu;
+  @FXML private Button addUserButton;
+
+  @FXML
+  private void handleAddUser () {
+    if (!addUsernameField.getText().isEmpty() && !addPasswordField.getText().isEmpty()) {
+      //Handle adding password
+    }
+
+  }
+}
