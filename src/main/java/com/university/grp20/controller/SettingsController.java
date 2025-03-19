ced1fcd5e57f4334375aadb77610f42d68d00dc7
Alex Peaty <alexpeaty1@gmail.com>
2025-03-19 17:27:40 +0000

    feat: adding pdf and csv exporting functionality for user logs

--- src/main/java/com/university/grp20/controller/SettingsController.java
+++ src/main/java/com/university/grp20/controller/SettingsController.java
@@ -28,6 +28,155 @@ public class SettingsController {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
 
+    private final Logger logger = LogManager.getLogger(MetricsController.class);
+
+    @FXML private TextField addUsernameField;
+    @FXML private TextField addPasswordField;
+    @FXML private ComboBox selectRoleMenu;
+    @FXML private Button addUserButton;
+    @FXML private HBox userManagementTitleBox;
+    @FXML private GridPane userManagementGridPane;
+    @FXML private ScrollPane settingsScrollPane;
+    @FXML private GridPane userEditGridPane;
+    @FXML private ComboBox selectUserMenu;
+    @FXML private TextField newPasswordField;
+    @FXML private ComboBox selectNewRoleMenu;
+    @FXML private Label currentPasswordLabel;
+    @FXML private Label currentRoleLabel;
+    @FXML private RadioButton pagesViewedOpt;
+    @FXML private RadioButton timeSpentOpt;
+    @FXML private Button bounceChooser;
+    @FXML private ToggleGroup bounceGroup;
+
+    private LoginService loginService = new LoginService();
+    private OperationLogger OperationLogger = new OperationLogger();
+
+    @FXML
+    private void initialize() {
+      operationLogger.log("Settings page clicked and displayed");
+      selectRoleMenu.getItems().addAll("Viewer", "Editor", "Admin");
+      selectNewRoleMenu.getItems().addAll("Viewer", "Editor", "Admin");
+      bounceGroup = new ToggleGroup();
+      pagesViewedOpt.setToggleGroup(bounceGroup);
+      timeSpentOpt.setToggleGroup(bounceGroup);
+
+      ArrayList<String> userList = loginService.getAllUsers();
+
+      // Add existing users in the database to the selectUserMenu
+      for (String user : userList) {
+        logger.info("Found user: " + user);
+        selectUserMenu.getItems().add(user);
+      }
+
+      // If user isn't an admin them remove all of the admin only settings
+      if (!User.getRole().equals("Admin")) {
+          VBox content = (VBox) settingsScrollPane.getContent();
+          content.getChildren().removeAll(userManagementTitleBox,userManagementGridPane,userEditGridPane);
+      }
+    }
+
+
+    @FXML
+    private void handleAddUser () {
+      logger.info("roleMenu had selected " + selectRoleMenu.getValue());
+
+      String enteredUsername = addUsernameField.getText();
+      String enteredPassword = addPasswordField.getText();
+
+      if (!enteredUsername.isEmpty() && !enteredPassword.isEmpty() && selectRoleMenu.getValue() != null) {
+        if (!loginService.doesUserExist(addUsernameField.getText())) {
+
+            boolean bSuccessful = loginService.addUser(enteredUsername, enteredPassword, selectRoleMenu.getValue().toString());
+
+            if (bSuccessful) {
+              Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+              alert.setTitle("Confirmation");
+              alert.setHeaderText(null);
+              alert.setContentText("New user \"" + enteredUsername + "\" was successfully added to the database");
+              alert.showAndWait();
+
+              addUsernameField.setText("");
+              addPasswordField.setText("");
+              selectRoleMenu.getSelectionModel().clearSelection();
+
+              selectUserMenu.getItems().add(enteredUsername);
+
+            } else {
+              showError("Something went wrong when adding the user to the database.");
+            }
+
+
+
+
+        } else {
+          showError("A user with that username already exists in the database");
+        }
+      } else {
+        showError("You have not filled out all the user detail fields..");
+      }
+
+    }
+
+    @FXML
+    private void updateEditUserLabels() {
+      if (selectUserMenu.getValue() != null) {
+        currentPasswordLabel.setText("Current Password: " + loginService.getUserPassword(selectUserMenu.getValue().toString()));
+        currentRoleLabel.setText("Current Role: " + loginService.getUserRole(selectUserMenu.getValue().toString()));
+      }
+
+    }
+
+    @FXML
+    private void handleUserChange() {
+
+      String enteredPassword = newPasswordField.getText();
+
+      if (selectUserMenu.getValue() != null) {
+
+        String selectedUser = selectUserMenu.getValue().toString();
+
+        if (!enteredPassword.isEmpty()) {
+          boolean passwordUpdateSuccess = loginService.changeUserPassword(selectedUser, enteredPassword);
+
+          if (!passwordUpdateSuccess) {
+            showError("Something went wrong with updating the user's password");
+          }
+        }
+
+        if (selectNewRoleMenu.getValue() != null) {
+          String selectedRole = selectNewRoleMenu.getValue().toString();
+
+          boolean roleUpdateSuccess = loginService.changeUserRole(selectedUser, selectedRole);
+
+          if (!roleUpdateSuccess) {
+            showError("Something went wrong with updating the user's role");
+          }
+        }
+
+        newPasswordField.setText("");
+        selectUserMenu.getSelectionModel().clearSelection();
+        selectNewRoleMenu.getSelectionModel().clearSelection();
+        currentPasswordLabel.setText("Current Password: ");
+        currentRoleLabel.setText("Current Role: ");
+
+      } else {
+        showError("You have not selected a user to edit");
+      }
+    }
+
+    @FXML
+    private void handleBack() {
+      UIManager.switchScene(UIManager.createFXMLLoader("/fxml/MetricsScene.fxml"));
+    }
+
+    private void showError(String errorMessage) {
+      Alert alert = new Alert(Alert.AlertType.ERROR);
+      alert.setTitle("Error!");
+      alert.setHeaderText(null);
+      alert.setContentText(errorMessage);
+      alert.showAndWait();
+    }
+
     @FXML
     public void handleExportLogToPDF(ActionEvent event) {
         String logFileName = operationLogger.getLogFileName();
@@ -55,153 +204,4 @@ public class SettingsController {
         alert.setContentText(message);
         alert.showAndWait();
     }
-
-  private final Logger logger = LogManager.getLogger(MetricsController.class);
-
-  @FXML private TextField addUsernameField;
-  @FXML private TextField addPasswordField;
-  @FXML private ComboBox selectRoleMenu;
-  @FXML private Button addUserButton;
-  @FXML private HBox userManagementTitleBox;
-  @FXML private GridPane userManagementGridPane;
-  @FXML private ScrollPane settingsScrollPane;
-  @FXML private GridPane userEditGridPane;
-  @FXML private ComboBox selectUserMenu;
-  @FXML private TextField newPasswordField;
-  @FXML private ComboBox selectNewRoleMenu;
-  @FXML private Label currentPasswordLabel;
-  @FXML private Label currentRoleLabel;
-  @FXML private RadioButton pagesViewedOpt;
-  @FXML private RadioButton timeSpentOpt;
-  @FXML private Button bounceChooser;
-  @FXML private ToggleGroup bounceGroup;
-
-  private LoginService loginService = new LoginService();
-  private OperationLogger operationLogger = new OperationLogger();
-
-  @FXML
-  private void initialize() {
-    operationLogger.log("Settings page clicked and displayed");
-    selectRoleMenu.getItems().addAll("Viewer", "Editor", "Admin");
-    selectNewRoleMenu.getItems().addAll("Viewer", "Editor", "Admin");
-    bounceGroup = new ToggleGroup();
-    pagesViewedOpt.setToggleGroup(bounceGroup);
-    timeSpentOpt.setToggleGroup(bounceGroup);
-
-    ArrayList<String> userList = loginService.getAllUsers();
-
-    // Add existing users in the database to the selectUserMenu
-    for (String user : userList) {
-      logger.info("Found user: " + user);
-      selectUserMenu.getItems().add(user);
-    }
-
-    // If user isn't an admin them remove all of the admin only settings
-    if (!User.getRole().equals("Admin")) {
-        VBox content = (VBox) settingsScrollPane.getContent();
-        content.getChildren().removeAll(userManagementTitleBox,userManagementGridPane,userEditGridPane);
-    }
-  }
-
-
-  @FXML
-  private void handleAddUser () {
-    logger.info("roleMenu had selected " + selectRoleMenu.getValue());
-
-    String enteredUsername = addUsernameField.getText();
-    String enteredPassword = addPasswordField.getText();
-
-    if (!enteredUsername.isEmpty() && !enteredPassword.isEmpty() && selectRoleMenu.getValue() != null) {
-      if (!loginService.doesUserExist(addUsernameField.getText())) {
-
-          boolean bSuccessful = loginService.addUser(enteredUsername, enteredPassword, selectRoleMenu.getValue().toString());
-
-          if (bSuccessful) {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Confirmation");
-            alert.setHeaderText(null);
-            alert.setContentText("New user \"" + enteredUsername + "\" was successfully added to the database");
-            alert.showAndWait();
-
-            addUsernameField.setText("");
-            addPasswordField.setText("");
-            selectRoleMenu.getSelectionModel().clearSelection();
-
-            selectUserMenu.getItems().add(enteredUsername);
-
-          } else {
-            showError("Something went wrong when adding the user to the database.");
-          }
-
-
-
-
-      } else {
-        showError("A user with that username already exists in the database");
-      }
-    } else {
-      showError("You have not filled out all the user detail fields..");
-    }
-
-  }
-
-  @FXML
-  private void updateEditUserLabels() {
-    if (selectUserMenu.getValue() != null) {
-      currentPasswordLabel.setText("Current Password: " + loginService.getUserPassword(selectUserMenu.getValue().toString()));
-      currentRoleLabel.setText("Current Role: " + loginService.getUserRole(selectUserMenu.getValue().toString()));
-    }
-
-  }
-
-  @FXML
-  private void handleUserChange() {
-
-    String enteredPassword = newPasswordField.getText();
-
-    if (selectUserMenu.getValue() != null) {
-
-      String selectedUser = selectUserMenu.getValue().toString();
-
-      if (!enteredPassword.isEmpty()) {
-        boolean passwordUpdateSuccess = loginService.changeUserPassword(selectedUser, enteredPassword);
-
-        if (!passwordUpdateSuccess) {
-          showError("Something went wrong with updating the user's password");
-        }
-      }
-
-      if (selectNewRoleMenu.getValue() != null) {
-        String selectedRole = selectNewRoleMenu.getValue().toString();
-
-        boolean roleUpdateSuccess = loginService.changeUserRole(selectedUser, selectedRole);
-
-        if (!roleUpdateSuccess) {
-          showError("Something went wrong with updating the user's role");
-        }
-      }
-
-      newPasswordField.setText("");
-      selectUserMenu.getSelectionModel().clearSelection();
-      selectNewRoleMenu.getSelectionModel().clearSelection();
-      currentPasswordLabel.setText("Current Password: ");
-      currentRoleLabel.setText("Current Role: ");
-
-    } else {
-      showError("You have not selected a user to edit");
-    }
-  }
-
-  @FXML
-  private void handleBack() {
-    UIManager.switchScene(UIManager.createFXMLLoader("/fxml/MetricsScene.fxml"));
-  }
-
-  private void showError(String errorMessage) {
-    Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setTitle("Error!");
-    alert.setHeaderText(null);
-    alert.setContentText(errorMessage);
-    alert.showAndWait();
-  }
 }
